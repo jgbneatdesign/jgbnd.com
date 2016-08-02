1c4301b73a2dd4ba73321edc30989e923cc1aaed
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-08-02 14:27:59 -0500

    work in progress

--- gulpfile.js
+++ gulpfile.js
@@ -9,24 +9,25 @@ elixir(function(mix) {
     var env = argv.e || argv.env || 'local';
     var port = argv.p || argv.port || 3000;
 
-   mix.copy('vendor/bower/jquery/dist/jquery.min.js', 'source/_assets/js/vendor/jquery.js')
+   	mix
+   		.copy('vendor/bower/jquery/dist/jquery.min.js', 'source/_assets/js/vendor/jquery.js')
 		.less('app.less')
 
-     	.scripts([
-     		'vendor/jquery.js',
-         	'uikit/uikit.min.js',
-         	'uikit/components/slideshow.min.js',
-         	'uikit/components/sticky.min.js',
-         	'uikit/components/slideshow-fx.min.js',
-         	// 'app.js',
-     	], 'source/js/app.js')
+	     	.scripts([
+	     		'vendor/jquery.js',
+	         	'uikit/uikit.min.js',
+	         	'uikit/components/slideshow.min.js',
+	         	'uikit/components/sticky.min.js',
+	         	'uikit/components/slideshow-fx.min.js',
+	         	// 'app.js',
+	     	], 'source/js/app.js')
 
-     // .version(['css/app.css', 'js/all.js'])
-     .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
-     .browserSync({
-         port: port,
-         server: { baseDir: 'build_' + env },
-         proxy: null,
-         files: [ 'build_' + env + '/**/*' ]
-     });
+	     // .version(['css/app.css', 'js/all.js'])
+	     .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+	     .browserSync({
+	         port: port,
+	         server: { baseDir: 'build_' + env },
+	         proxy: null,
+	         files: [ 'build_' + env + '/**/*' ]
+	     });
 });
